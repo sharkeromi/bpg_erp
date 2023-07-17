989b3636f42eb8c72f0cfacdd9ebd791a497ed40
sharkeromi <genie.sharkeromi@gmail.com>
2023-07-17 18:11:27 +0600

    imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:bpg_erp/utils/getx_dependencies.dart';
-import 'package:bpg_erp/views/bgp_login_page.dart';
 import 'package:bpg_erp/views_beta/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
