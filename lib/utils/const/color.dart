e3da09126c6514599f9956b2ffd7fb072c9f773a
wahidmurad1 <genie.murad@gmail.com>
2023-07-17 18:19:59 +0600

    ---

---
+++ lib/utils/const/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const scaffoldColor = Colors.black;
+
+const blackColor = Colors.black;
+
+const whiteColor = Colors.white;
+
+const redColor = Colors.red;
+
+const backgroundColor = Color(0XFFe7f0f9);
