45d07c34aa001dc5596b4e49eb8d77b712809b60
Zahid046 <zmemon046@gmail.com>
2023-08-23 10:19:23 +0600

    env setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
+import 'package:bpg_erp/Service/environment.dart';
 import 'package:bpg_erp/controller/binder/getx_dependencies.dart';
+import 'package:bpg_erp/utils/const/value.dart';
 import 'package:bpg_erp/views/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   AllControllerBinder().dependencies();
+
+  await dotenv.load(fileName: Environment.fileName);
+  ll(Environment.fileName.toString());
+
+  
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
